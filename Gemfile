dcb63af542e3e605d46bc656a7380d0c1fa1bdde
Kate Lynch <katelynch@princeton.edu>
2022-08-03 15:28:05 -0400

    Removed pry, made rubocop fixes

--- Gemfile
+++ Gemfile
@@ -1,7 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'nokogiri'
-
-gem 'pry'
-
 gem 'rubocop'
--- lookup.rb
+++ lookup.rb
@@ -1,4 +1,6 @@
-#!/usr/bin/ruby
+# frozen_string_literal: true
+
+# !/usr/bin/ruby
 
 require 'net/http'
 require 'uri'
@@ -6,46 +8,35 @@ require 'json'
 require 'openssl'
 require 'nokogiri'
 
-require 'pry'
+HOST = 'https://dataspace.princeton.edu'
 
-HOST = "https://dataspace.princeton.edu"
-
-
-def fetch_response(title, language = "")
+def fetch_response(title, language = '')
   uri = URI.parse("#{HOST}/rest/items/find-by-metadata-field")
   request = Net::HTTP::Post.new(uri)
-  request.content_type = "application/json"
-  request["Accept"] = "application/json"
-  request.body = JSON.dump({
-    "key" => "dc.title",
-    "value" => title,
-    "language" => language
-  })
-
+  request.content_type = 'application/json'
+  request['Accept'] = 'application/json'
+  request.body = JSON.dump({ 'key' => 'dc.title', 'value' => title, 'language' => language })
   req_options = {
-    use_ssl: uri.scheme == "https",
-    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+    use_ssl: uri.scheme == 'https',
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
   }
 
-  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
     http.request(request)
   end
-
-
 end
 
 def lookup_by_title(title)
-
   response = fetch_response(title)
 
-  languages = ["", "en", "en_US"]
+  languages = ['', 'en', 'en_US']
 
   languages.each do |language|
     response = fetch_response(title, language)
-    break if response.body != "[]"
+    break if response.body != '[]'
   end
 
-  if response.code == "200"
+  if response.code == '200'
 
     results = JSON.parse(response.body)
 
@@ -54,12 +45,10 @@ def lookup_by_title(title)
       result_response = Net::HTTP.get_response(result_uri)
       xml_payload = JSON.parse(result_response.body)
 
-      if xml_payload.select{ |entries| entries["key"]== "dc.type" && entries["value"] == "Academic dissertations (Ph.D.)"}.length == 1
-        ark = xml_payload.select{ |entries| entries["key"]== "dc.identifier.uri"}.first["value"]
-        ark.slice!("http://arks.princeton.edu/")
-
+      if xml_payload.select { |entries| entries['key'] == 'dc.type' && entries['value'] == 'Academic dissertations (Ph.D.)' }.length == 1
+        ark = xml_payload.select { |entries| entries['key'] == 'dc.identifier.uri' }.first['value']
+        ark.slice!('http://arks.princeton.edu/')
         puts "#{title}\t#{ark}\n"
-
       else
         puts "#{title} - Match found in DataSpace but *NOT* a dissertation, skipping..."
       end
